cb45b8717b4c8e82ce96ce832d5d9f68e458c72e
YaSielDeda <bereg6484@gmail.com>
2020-11-22 17:17:58 +0400

    don3

--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -19,7 +19,7 @@ public class Test_1 {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://pokupki.market.yandex.ru/");
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement exit = driver.findElement(By.xpath("//*[text()='Дальше']")); // Всплыв. окно
         exit.click();
         exit.click();
@@ -49,7 +49,7 @@ public class Test_1 {
         }
 
         //Test_2
-        WebElement city = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div/div/div/div/div[2]/div[2]/div[2]/div/div/div[1]/div[2]/span"));
+        WebElement city = driver.findElement(By.xpath("//span[@class=\"b_3PaKLyf5hk\"]"));
         city.click();
         city = driver.findElementByCssSelector("input[type=\"text\"]");
         city.click();
@@ -72,7 +72,12 @@ public class Test_1 {
         city = driver.findElementByCssSelector("button[data-auto=\"ok\"]");
         city.click();
 
-        driver.findElement(By.cssSelector("a[href=\"/my/settings\"]")).click();
+        WebElement el = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href=\"/my/settings\"]")));
+
+        city = driver.findElement(By.xpath("//a[@href=\"/my/settings\"]"));
+        city.click();
+
         //-----
         ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(0));
@@ -92,25 +97,46 @@ public class Test_1 {
             System.out.println("Выставлен другой город");
         }
 
-        WebElement tovar = driver.findElementByCssSelector("div[data-zone-name=\"headerCatalog\"] button");
+        //WebElement tovar = driver.findElementByCssSelector("div[data-zone-name=\"headerCatalog\"] button");
+        //tovar.click();
+
+        // //a[@href]//*[contains(text(),"Гигиена")][1]
+        // //a[@title="Гигиена"]
+        //tovar = driver.findElement(By.xpath("//a[@href]//*[contains(text(),\"Гигиена\")][1]"));
+        //tovar.click();
+
+        //Test_3
+        WebElement tovar = driver.findElement(By.xpath("//a[@href=\"//market.yandex.ru/\"][1]"));
         tovar.click();
 
-        tovar = driver.findElement(By.cssSelector("a[href=\"/catalog/gigiena-v-khvalynske/18049517?hid=91172\"]"));
+        tovar = driver.findElement(By.cssSelector("div[data-zone-data='{\"index\":6,\"title\":\"Гигиена\"}']")); // "Гигиена" на главной странице
         tovar.click();
+        //------
 
-        tovar = driver.findElement(By.xpath("//span[contains(text(),'Показать все')]"));
+        //tovar = driver.findElement(By.xpath("//a[text()=\"Уход за полостью рта\"]/../..//*[text()=\"Ещё\"]"));
+        //tovar.click();
+
+        tovar = driver.findElement(By.xpath("//span[text()=\"Показать все\"]"));
         tovar.click();
 
         driver.findElement(By.xpath("//*[text()='Электрические зубные щетки']")).click();
-        //Остановился тут
-        tovar = driver.findElement(By.xpath("//div[@data-auto=\"filter-range-glprice\"]/ div /span /div /div /input"));
+
+        /*tovar = driver.findElement(By.xpath("//input[@name=\"Цена от\"]"));
         tovar.click();
 
         tovar.sendKeys("999");
 
-        tovar = driver.findElement(By.xpath("//span[@data-auto=\"filter-range-max\"] /div /div /input"));
+        tovar = driver.findElement(By.xpath("//input[@name=\"Цена до\"]"));
         tovar.click();
 
+        tovar.sendKeys("1999");*/
+
+        tovar = driver.findElement(By.xpath("//*[@data-auto=\"filter-range-min\"]//input[1]"));
+        tovar.click();
+        tovar.sendKeys("999");
+
+        tovar = driver.findElement(By.xpath("//*[@data-auto=\"filter-range-max\"]//input[1]"));
+        tovar.click();
         tovar.sendKeys("1999");
 
         WebElement check;
@@ -122,7 +148,43 @@ public class Test_1 {
             System.out.println("Фильтр отсутствует");
         }
 
-        tovar = driver.findElement(By.xpath("//div[@data-auto=\"pagination-page\" and text()='3']"));
+        /*WebElement wait = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@data-zone-name=\"snippetList\"]/*[position() = last()-1]//*[contains(text(), \"зубная щетка\")]")));
+
+        tovar = driver.findElement(By.xpath("//div[@data-zone-name=\"snippetList\"]/*[position() = last()-1]//*[contains(text(), \"зубная щетка\")]"));
+        tovar.click();*/
+
+        //tovar = driver.findElement(By.xpath("//*[@data-auto=\"SerpPage\"]/*/*[last() - 1] //button"));
+
+        //tovar = driver.findElement(By.xpath("//*[@id="1-5"]/*/*/*[3]//button"));
+        //tovar.click();
+
+        Thread.sleep(2000);
+
+        try{
+            tovar = driver.findElement(By.xpath("//*[@id='1-5']/*/*/*[3]//button"));
+            tovar.click();
+        }catch (org.openqa.selenium.NoSuchElementException ex){
+            tovar = driver.findElement(By.xpath("//*[@data-auto=\"SerpPage\"]/*/*[last() - 1] //button"));
+            tovar.click();
+        }
+
+        dynamicElement = (new WebDriverWait(driver, 10)) // Ожидание сообщения что продукт добавлен в корзину
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-auto=\"executed-cart-button\"]")));
+
+        //tovar = driver.findElement(By.xpath("//button[@data-auto=\"executed-cart-button\"]"));
+        //tovar.click();
+
+        //tovar = driver.findElement(By.xpath("//a[@href=\"/my/cart\"]"));
+        //tovar.click();
+
+        try{
+            driver.findElement(By.xpath("//a[@href=\"/my/cart\"]")).click();
+        }catch(org.openqa.selenium.ElementClickInterceptedException ex){
+            driver.findElement(By.xpath("//*[text()=\"Перейти в корзину\"]/..")).click();
+        }
+
+        /*tovar = driver.findElement(By.xpath("//div[@data-auto=\"pagination-page\" and text()='3']"));
         tovar.click();
 
         tovar = driver.findElement(By.xpath("//*[contains(text(),\"Электрическая зубная щетка URM SO WHITE, с подставкой, синяя\")]/../../../../../../div[3]/button"));
@@ -133,20 +195,30 @@ public class Test_1 {
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[class=\"b_2AzvrhMLkq b_3NkKQ6Z6Ij\"]")));
 
         tovar = driver.findElement(By.cssSelector("a[href=\"/my/cart\"]"));
-        tovar.click();
+        tovar.click();*/
 
         // -------
-        tovar = driver.findElement(By.xpath("//div[@data-auto=\"total-price\"]/span[2]"));
+        Thread.sleep(2000);
+        tovar = driver.findElement(By.xpath("//*[text()='Итого']/../span[2]"));
         String temp = tovar.getText();
         String res = temp.replaceAll("[' ', ₽]", "");
 
         while(Integer.parseInt(res) < 2999)
         {
-            driver.findElement(By.xpath("//*[text()='+']")).click();
-            temp = tovar.getText();
-            res = temp.replaceAll("[' ', ₽]", "");
+            // Если элемент, предупреждающий о том, что данного товара в наличии больше нет отсутствует, то цикл прерывается
+            try{
+                driver.findElement(By.xpath("//*[contains(text(),\"штук\")]"));
+                break;
+            }catch (NoSuchElementException ex){
+                driver.findElement(By.xpath("//*[text()='+']")).click();
+
+                Thread.sleep(3000);
+
+                temp = tovar.getText();
+                res = temp.replaceAll("[' ', ₽]", "");
+            }
         }
 
-        driver.quit();
+        //driver.quit();
     }
 }
